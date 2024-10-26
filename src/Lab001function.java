59ab7034b7af99e1e0dddb19ef2317a53658d556
Aditi <aditigawade78@gmail.com>
2024-10-26 19:40:38 +0530

    https://github.com/Aditi-oss/Interview-questions.git

---
+++ src/Lab001function.java
@@ -0,0 +1,30 @@
+public class Lab001function {
+    public static void main(String[]args)
+    {
+//public returnType functionName(parametertype parameterName)
+
+            //body of function
+            //builtin function
+            //user defined function
+            int result=Math.max(3,4);
+            System.out.println(result);
+            //define the  function
+            //calling the function
+            //without parameter and return type
+            //without parameter but return type
+            //with parameters and without return type
+            //
+        greet();
+        greet();
+        for (int i=0;i<=99;i++)
+        greet();
+        }
+//outside the main method
+static void greet()
+{
+    System.out.println("how are you");
+}
+    }
+
+
+
